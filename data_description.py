a4069936f44259484ccc00d87d93755b801343bb
beratyavas19 <i.beratyavas@gmail.com>
2020-07-13 20:52:13 +0300

    data_description dosyasi eklendi

---
+++ data_description.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+train_values = pd.read_csv('Data/train_values.csv', index_col='building_id')
+train_labels = pd.read_csv('Data/train_labels.csv', index_col='building_id')
+print(train_values.dtypes)
+(train_labels.damage_grade
+             .value_counts()
+             .sort_index()
+             .plot.bar(title="Number of Buildings with Each Damage Grade"))
+selected_features = ['foundation_type',
+                     'area_percentage',
+                     'height_percentage',
+                     'count_floors_pre_eq',
+                     'land_surface_condition',
+                     'has_superstructure_cement_mortar_stone']
+
+train_values_subset = train_values[selected_features]
+sns.pairplot(train_values_subset.join(train_labels),
+             hue='damage_grade')
