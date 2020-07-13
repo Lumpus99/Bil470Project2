7daba6097c6625c0547c9267e656b988dc133a16
beratyavas19 <i.beratyavas@gmail.com>
2020-07-13 21:08:44 +0300

    data_description.py

--- data_description.py
+++ data_description.py
@@ -3,8 +3,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-train_values = pd.read_csv('Data/train_values.csv', index_col='building_id')
-train_labels = pd.read_csv('Data/train_labels.csv', index_col='building_id')
+train_values = pd.read_csv('data/train_values.csv', index_col='building_id')
+train_labels = pd.read_csv('data/train_labels.csv', index_col='building_id')
 print(train_values.dtypes)
 (train_labels.damage_grade
              .value_counts()
@@ -18,5 +18,5 @@ selected_features = ['foundation_type',
                      'has_superstructure_cement_mortar_stone']
 
 train_values_subset = train_values[selected_features]
-sns.pairplot(train_values_subset.join(train_labels),
-             hue='damage_grade')
+sns.pairplot(train_values_subset.join(train_labels),hue='damage_grade')
+
