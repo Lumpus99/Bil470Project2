241168f51b58652cedb4cde7660aeb36e461bd28
Lumpus99 <cakiroglu.mert@gmail.com>
2020-07-13 23:20:32 +0300

    Fixed col

--- Random_Forest.py
+++ Random_Forest.py
@@ -7,18 +7,23 @@ train_target = pd.read_csv('data/train_labels.csv')['damage_grade']
 
 le = preprocessing.LabelEncoder()
 
-train_data = np.genfromtxt('data/train_values.csv', delimiter=",", dtype="|a20", skip_header=1)
-test_values = np.genfromtxt('data/test_values.csv', delimiter=",", dtype="|a20", skip_header=1)
+cols = tuple(range(1, 39))
+
+train_data = np.genfromtxt('data/train_values.csv', delimiter=",", dtype="|a20", skip_header=1, usecols=cols)
+test_values = np.genfromtxt('data/test_values.csv', delimiter=",", dtype="|a20", skip_header=1, usecols=cols)
+
+print(type(train_data), train_data)
 
 # 39 = attribute number
-for i in range(39):
+for i in range(38):
     train_data[:, i] = le.fit_transform(train_data[:, i])
 
-for i in range(39):
+for i in range(38):
     test_values[:, i] = le.fit_transform(test_values[:, i])
 
-test_values = test_values[0]
-test_values = np.array([test_values])
+test_values = test_values
+test_values = np.array(test_values)
+
 
 rf = RandomForestClassifier()
 
