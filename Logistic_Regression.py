3d0c48f95749d78105904c4d457470ba30d0446e
Lumpus99 <cakiroglu.mert@gmail.com>
2020-07-18 20:58:43 +0300

    Added Logistic_Regression.py

--- Logistic_Regression.py
+++ Logistic_Regression.py
@@ -1,8 +1,8 @@
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 import Readfile as Rf
 
 x_train, x_test, y_train, y_test = Rf.read_data()
-random_forest = RandomForestClassifier()
+random_forest = LogisticRegression()
 random_forest.fit(x_train, y_train)
 predictions = random_forest.predict(x_test)
 Rf.calc_success(predictions, y_test)
