be307ae428709989b87d21fc6bfa684b3378b891
Lumpus99 <cakiroglu.mert@gmail.com>
2020-07-19 00:15:02 +0300

    Completed Blending.py

--- Blending.py
+++ Blending.py
@@ -11,19 +11,24 @@ def do_blending():
 
     print("Staring Random Forest:")
     rf_model = read_model("Random_Forest")
-    rf_predictions = rf_model.predict(x_test)
+    rf_predictions_train = rf_model.predict(x_train)
+    rf_predictions_test = rf_model.predict(x_test)
 
     print("Staring SVM:")
     svm_model = read_model("SVM")
-    svm_predictions = svm_model.predict(x_test)
+    svm_predictions_train = svm_model.predict(x_train)
+    svm_predictions_test = svm_model.predict(x_test)
 
     print("Staring AdaBoost:")
     ada_model = read_model("Ada")
-    ada_predictions = ada_model.predict(x_test)
+    ada_predictions_train = ada_model.predict(x_train)
+    ada_predictions_test = ada_model.predict(x_test)
 
     print("Staring Blending:")
-
-    Lr.do_logistic_regression(ada_predictions, rf_predictions, svm_predictions, x_test, y_train, y_test)
+    Lr.do_logistic_regression(ada_predictions_train, ada_predictions_test,
+                              rf_predictions_train, rf_predictions_test,
+                              svm_predictions_train, svm_predictions_test,
+                              y_train, y_test)
 
 
 def main():
@@ -32,4 +37,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
--- Logistic_Regression.py
+++ Logistic_Regression.py
@@ -3,15 +3,15 @@ import Readfile as Rf
 import numpy as np
 
 
-def do_logistic_regression(ada, rf, svm, x_test, y_train, y_test):
-    print("LR")
-    x_train = np.array(ada, rf, svm)
-    print(x_train)
-    # _, x_test, y_train, y_test = Rf.read_data()
-    print("asad")
-    random_forest = LogisticRegression()
-    print("asad1")
-    random_forest.fit(x_train, y_train)
-    print("asad2")
-    predictions = random_forest.predict(x_test)
-    Rf.calc_success(predictions, y_test)
+def do_logistic_regression(ada_train, ada_test, rf_train, rf_test, svm_train, svm_test, y_train, y_test):
+    x_train = [ada_train, rf_train, svm_train]
+    x_train = np.array(x_train).transpose()
+
+    x_test = [ada_test, rf_test, svm_test]
+    x_test = np.array(x_test).transpose()
+
+    logistic_regression = LogisticRegression()
+    logistic_regression.fit(x_train, y_train)
+
+    result = logistic_regression.score(x_test, y_test)
+    print(result)
