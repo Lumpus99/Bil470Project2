8016017555a72c935de9dac8ed3f46ab4b3ee505
Lumpus99 <cakiroglu.mert@gmail.com>
2020-07-18 20:46:13 +0300

    Improved svm

--- svm.py
+++ svm.py
@@ -1,11 +1,14 @@
 import Readfile as Rf
 from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
 
 
 x_train, x_test, y_train, y_test = Rf.read_data()
+
+scaler = StandardScaler()
+x_train = scaler.fit(x_train).transform(x_train)
+
 svc = SVC(kernel='linear', cache_size=7000, max_iter=1000)
-print("done1")
 svc.fit(x_train, y_train)
 predictions = svc.predict(x_test)
-print(predictions)
 Rf.calc_success(predictions, y_test)
