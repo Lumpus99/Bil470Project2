9ccf679a0956173590f31caa3d08baf4d427c51a
Grisworld <oguzandas@gmail.com>
2020-07-19 15:52:49 +0300

    Added KFOLD

--- helloworld.py
+++ helloworld.py
@@ -3,13 +3,5 @@ from Random_Forest import do_random_forest
 from Adaboost import do_ada_boost
 from Grafient_boost import do_gradient_boost
 
-do_gradient_boost()
 
-"""print("Rf")
-do_random_forest()
-print("Svm")
-do_svm()
-print("Ada")
-do_ada_boost()
-print("Done!")
-"""
+do_gradient_boost()
